ec9552316a49f00cb85a964da2189767cf0efd03
Patrick Lane <patricklane09@gmail.com>
2022-06-06 18:10:19 -0500

    Create function to reverse given string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let chars = str.split('');
+    let revStr = '';
 
+    chars.reverse();
+    
+    for(char in chars){
+        revStr += chars[char];
+    }
+    console.log(revStr);
+    return revStr;
 };
 
 // Do not edit below this line
