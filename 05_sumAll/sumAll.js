9260253bd7b2e8b9f5962c024f0d508e9e36c177
Patrick Lane <patricklane09@gmail.com>
2022-06-07 12:51:54 -0500

    Implement function to sum values between two nums

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    if(typeof(num1) != 'number' || typeof(num2) != 'number'){
+        return 'ERROR'
+    }else if(num1 < 0 || num2 < 0){
+        return 'ERROR'
+    }else if(num1 > num2){
+        for(num2; num2 <= num1; num2++){
+            sum += num2;
+        }
+        return sum
+    }else{
+        for(num1; num1 <= num2; num1++){
+            sum += num1;  
+        }
+        return sum
+    }
+    
 };
 
 // Do not edit below this line
