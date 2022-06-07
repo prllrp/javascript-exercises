0d9eda9c9539f43c7e621e4981e98583ae74912c
Patrick Lane <patricklane09@gmail.com>
2022-06-06 22:02:46 -0500

    Removes items from an array This function filters in a loop all characters given

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    const inputs = [...arguments];
+    let newArr = [];
+    let arr = inputs[0];
+    for(let i = 0;i < inputs.length - 1 ; i++){
+       const newArr =  arr.filter(num => num !== inputs[i+1])
+       arr = newArr;
+       console.log(arr)
+    }
+    
+    return arr;
+
+    
 
 };
 
