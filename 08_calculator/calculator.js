58f9197fddcbda968e48a0dd365ed1258fab61da
Patrick Lane <patricklane09@gmail.com>
2022-06-21 14:03:02 -0500

    Solutions for all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	const product = nums.reduce((sum,num)=>{
+    sum += num;
+    return sum;
+  },0);
+  return product;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  const product = nums.reduce((product,num)=>{
+    product *= num;
+    return product;
+  },1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	let total = 1;
+  for(power; power > 0; power --){
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0){
+    return 1
+  }
+  let total = 1;
+  for(num; num > 0; num -- ){
+    total *= num;
+  }
+  return total;
 };
 
 // Do not edit below this line
