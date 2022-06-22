e263f841dc7b4054471013984d28e5468eed42c6
Patrick Lane <patricklane09@gmail.com>
2022-06-22 15:59:16 -0500

    Finally got it done, this took way too long

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const characters = word.toLowerCase().split('');
+    const filtered = characters.filter(element => {
+        if(element === ',' || element === '.' ||element === '!' ||element === ';' || element === '?' || element === ' '){
+            return false
+        }else{
+            return true
+        }
+    });
+    for(let i = 0; i < filtered.length; i++){
+        console.log(filtered[i], filtered[filtered.length - i - 1])
+        if(filtered[i] !== filtered[filtered.length - i - 1]){
+            return false
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
