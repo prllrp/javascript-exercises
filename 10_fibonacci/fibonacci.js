2e8d3126e31db2779864c958892cd285b4a08684
Patrick Lane <patricklane09@gmail.com>
2022-06-22 16:14:17 -0500

    Implemented fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS"
+    }
+    let fib = [0,1];
+    for(let i= 1; i <= num; i++){
+        fib.push(fib[i] + fib[i-1])
+        console.log(fib)
+    }
+    return fib[num]
 };
 
 // Do not edit below this line
