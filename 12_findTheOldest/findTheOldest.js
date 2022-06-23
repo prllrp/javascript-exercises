9e0bbb59e407f3bceb6796e34bceb04543ad3c83
Patrick Lane <patricklane09@gmail.com>
2022-06-22 23:47:39 -0500

    Function that finds the oldest person in an array of people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+   const old = arr.reduce((oldest, person)=>{
+        let age = 0;
+        
+        if(!person.yearOfDeath){
+            age = new Date().getFullYear() - person.yearOfBirth;
+            console.log(new Date().getFullYear)
+        }else{
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+        person.age = age;
+        if (age > oldest.age || oldest.age === 0){
+            return person 
+        }else{
+            return oldest
+        }
+        
+    },{age:0})
+    console.log(old)
+    return old
 };
 
 // Do not edit below this line
